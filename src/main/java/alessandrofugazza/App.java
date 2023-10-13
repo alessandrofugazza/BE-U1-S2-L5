1caa885666b3abf159f3b3503ad8c4ed577ced95
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-13 09:22:19 +0200

    project initialization

---
+++ src/main/java/alessandrofugazza/App.java
@@ -0,0 +1,13 @@
+package alessandrofugazza;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
